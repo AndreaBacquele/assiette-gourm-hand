4d415c2bfe5c7876b5fd39fcf2443e7e31168a5c
Andréa Bacquelé <bacquelea@gmail.com>
2024-05-31 16:00:07 +0200

    feat: add cors and request in frontend

--- backend/app.js
+++ backend/app.js
@@ -1,71 +1,77 @@
-const express = require('express');
-const db = require('./db');
+const express = require("express");
+const db = require("./db");
 const app = express();
 const port = 4000;
-const cors = require('cors')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(cors());
 
-
 // Définition des routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/test-db-connection', async (req, res) => {
+app.get("/test-db-connection", async (req, res) => {
   try {
-    const client = await db.connect(); 
-    const result = await client.query('SELECT NOW()'); 
-    res.send(`La connexion à la base de données a réussi. Temps actuel : ${result.rows[0].now}`);
+    const client = await db.connect();
+    const result = await client.query("SELECT NOW()");
+    res.send(
+      `La connexion à la base de données a réussi. Temps actuel : ${result.rows[0].now}`
+    );
   } catch (err) {
-    console.error('Erreur lors de la connexion à la base de données:', err);
-    res.status(500).send('Impossible de se connecter à la base de données.');
+    console.error("Erreur lors de la connexion à la base de données:", err);
+    res.status(500).send("Impossible de se connecter à la base de données.");
   }
 });
 
-app.get('/jury', async (req, res) => {
+app.get("/jury", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM jury');
-    res.json(result.rows); 
+    const result = await db.query("SELECT * FROM jury");
+    res.json(result.rows);
   } catch (error) {
-    console.error(error);  
-    res.status(500).send('Une erreur est survenue lors de la récupération des données de jury.'); 
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "Une erreur est survenue lors de la récupération des données de jury."
+      );
   }
 });
-
 
 // Register jury
 
-app.post('/add-to-jury', async (req, res) => {
+app.post("/add-to-jury", async (req, res) => {
   try {
-    const { nom, mdp , type_epreuve_id} = req.body; 
+    const { nom, mdp, type_epreuve_id } = req.body;
     const saltRounds = 10;
     // const hashedMdp = await bcrypt.hash(mdp, saltRounds);
 
     const text = `
       INSERT INTO jury (nom, mdp, type_epreuve_id)
       VALUES ($1, $2, $3)
-      RETURNING *;`; 
+      RETURNING *;`;
 
     const result = await db.query(text, [nom, mdp, type_epreuve_id]);
 
-    res.status(201).json(result.rows[0])
+    res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Une erreur est survenue lors de l\'ajout d\'un membre au jury.');
+    res
+      .status(500)
+      .send("Une erreur est survenue lors de l'ajout d'un membre au jury.");
   }
 });
 
 // Login jury
 // TODO : add token
 
-app.post('/login-jury', async(req, res)=> {
+app.post("/login-jury", async (req, res) => {
   try {
-    const { nom, mdp } = req.body
-    const query = 'SELECT * FROM jury WHERE nom = $1 AND mdp = $2';
+    const { nom, mdp } = req.body;
+    const query = "SELECT * FROM jury WHERE nom = $1 AND mdp = $2";
     const values = [nom, mdp];
 
     const result = await db.query(query, values);
@@ -73,14 +79,18 @@ app.post('/login-jury', async(req, res)=> {
     if (result.rows.length > 0) {
       res.status(200).json(result.rows[0]);
     } else {
-      res.status(401).send('Nom ou mot de passe incorrect');
+      res.status(401).send("Nom ou mot de passe incorrect");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Une erreur est survenue lors de l\'authentification d\'un membre au jury.');
+    res
+      .status(500)
+      .send(
+        "Une erreur est survenue lors de l'authentification d'un membre au jury."
+      );
   }
 });
-    // Avec hashage du mot de passe 
+// Avec hashage du mot de passe
 //     if (result.rows.length > 0) {
 //       const user = result.rows[0];
 
@@ -99,59 +109,66 @@ app.post('/login-jury', async(req, res)=> {
 //   }
 // });
 
-
-
-
-
-
-app.get('/type_epreuve', async (req, res) => {
+app.get("/type_epreuve", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM type_epreuve');
-    res.json(result.rows); 
+    const result = await db.query("SELECT * FROM type_epreuve");
+    res.json(result.rows);
   } catch (error) {
-    console.error(error);  
-    res.status(500).send('Une erreur est survenue lors de la récupération des données de type_epreuve.'); 
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "Une erreur est survenue lors de la récupération des données de type_epreuve."
+      );
   }
 });
 
-// GESTION DES NOTES 
+// GESTION DES NOTES
 
-app.get('/notes_criteria', async (req, res) => {
+app.get("/notes_criteria", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM notes_criteria');
-    res.json(result.rows); 
+    const result = await db.query("SELECT * FROM notes_criteria");
+    res.json(result.rows);
   } catch (error) {
-    console.error(error);  
-    res.status(500).send('Une erreur est survenue lors de la récupération des données de criteria_notes.'); 
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "Une erreur est survenue lors de la récupération des données de criteria_notes."
+      );
   }
 });
 
-app.get('/notes', async (req, res) => {
+app.get("/notes", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM notes');
-    res.json(result.rows); 
+    const result = await db.query("SELECT * FROM notes");
+    res.json(result.rows);
   } catch (error) {
-    console.error(error);  
-    res.status(500).send('Une erreur est survenue lors de la récupération des données de notes.'); 
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "Une erreur est survenue lors de la récupération des données de notes."
+      );
   }
 });
 
 // TODO : add the jury_id
-app.post('/add-to-notes', async (req, res) => {
+app.post("/add-to-notes", async (req, res) => {
   try {
-    const { note, criteria_name , candidat_id} = req.body; 
+    const { note, criteria_name, candidat_id } = req.body;
 
     const text = `
       INSERT INTO notes (note, criteria_name, candidat_id)
       VALUES ($1, $2, $3)
-      RETURNING *;`; 
+      RETURNING *;`;
 
     const result = await db.query(text, [note, criteria_name, candidat_id]);
 
-    res.status(201).json(result.rows[0])
+    res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Une erreur est survenue lors de l\'ajout de la note.');
+    res.status(500).send("Une erreur est survenue lors de l'ajout de la note.");
   }
 });
 
