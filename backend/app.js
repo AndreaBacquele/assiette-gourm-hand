fe1e21a9601ad58289299f875ca600862c7c8535
Andréa Bacquelé <bacquelea@gmail.com>
2024-08-02 09:28:20 +0200

    chore: Add orm sequelize and first model

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const db = require('./db');
-const { connectToDatabase } = require("./sequelize");
+const { connectToDatabase, sequelize } = require("./sequelize");
+const Jury = require("./model");
 const app = express();
 const port = 4001;
 const cors = require("cors");
