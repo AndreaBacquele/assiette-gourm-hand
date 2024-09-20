b8820cb059844c6f5b4bef769ab98da4ee9655f9
Andréa Bacquelé <bacquelea@gmail.com>
2024-09-20 12:44:26 +0200

    add: new test for main page + config github action

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,7 +1,75 @@
 /// <reference types="cypress" />
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('ion-content', 'Tab 1 page')
-  })
-})
+
+describe('Form Validation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should not allow submission if jury type is not selected', () => {
+    cy.get('input[placeholder="Numéro de jury"]').type('123');
+    cy.get('input[placeholder="Prénom NOM"]').type('John Doe');
+
+    // Essaye de soumettre le formulaire sans sélectionner de type de jury
+    cy.get('form').submit();
+
+    // Vérifie que le bouton est désactivé
+    cy.get('button[type="submit"]').should('be.disabled');
+  });
+});
+
+describe('Form Submission', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should submit the form and redirect based on jury type', () => {
+    cy.get('input[placeholder="Numéro de jury"]').type('123');
+    cy.get('input[placeholder="Prénom NOM"]').type('John Doe');
+
+    // Sélectionne le type de jury
+    cy.get('input[type="radio"][value="Dégustation"]').check();
+
+    // Soumets le formulaire
+    cy.get('form').submit();
+
+    // Vérifie la redirection
+    cy.url().should('include', '/listingdegustation');
+  });
+
+  it('should submit the form and redirect to the technique listing', () => {
+    cy.get('input[placeholder="Numéro de jury"]').type('456');
+    cy.get('input[placeholder="Prénom NOM"]').type('Jane Doe');
+
+    // Sélectionne le type de jury
+    cy.get('input[type="radio"][value="Technique"]').check();
+
+    // Soumets le formulaire
+    cy.get('form').submit();
+
+    // Vérifie la redirection
+    cy.url().should('include', '/listingtechnique');
+  });
+});
+
+describe('Accueil Page Tests', () => {
+  beforeEach(() => {
+    // Visite la page d'accueil avant chaque test
+    cy.visit('/');
+  });
+
+  it('should display the logo', () => {
+    cy.get('.logo-accueil').should('be.visible');
+  });
+
+  it('should display the title', () => {
+    cy.get('#title').contains('Inscription des jurys');
+  });
+
+  it('should display the instructions', () => {
+    cy.get('#instructions').contains('Merci de compléter les informations ci-dessous afin d\'avoir accès à la liste des candidats et aux grilles d\'évaluation');
+  });
+
+  it('should display the validation message', () => {
+    cy.get('h6').contains('Toute validation est définitive, merci de bien vérifier les informations saisies avant de continuer.');
+  });
+});
