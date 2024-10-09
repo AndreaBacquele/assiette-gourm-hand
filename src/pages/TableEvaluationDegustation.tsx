8a84c1c30e8b99f64da900c280794669fbbcc978
Andréa Bacquelé <bacquelea@gmail.com>
2024-10-09 21:42:33 +0200

    send an alert if invalid notes

--- src/pages/TableEvaluationDegustation.tsx
+++ src/pages/TableEvaluationDegustation.tsx
@@ -21,6 +21,7 @@ function TableEvaluationDegustation() {
   // Récupére le numéro de candidat dans l'URL
   const { candidate } = useParams<{ candidate: string }>();
   const [alertNoSend, setAlertNoSend] = useState(false);
+  const [alertInvalideNotes, setAlertInvalideNotes] = useState(false);
 
   const { store } = useStorage();
   //Récupération des informations des inputs
@@ -60,33 +61,75 @@ function TableEvaluationDegustation() {
   const history = useHistory();
   const [validateNote, setValidateNote] = useState(false);
   // Stockage des notes dés que l'on appuie sur le bouton Valider l'évaluation
-  const handleValidateClick = () => {
-    if (store) {
-      let candidates_notes = {
-        presentation: values.presentation,
-        cuissonPrincipale: values.cuissonPrincipale,
-        cuissonGarniture: values.cuissonGarniture,
-        accordGlobal: values.accordGlobal,
-        total: total,
-        observations: observations,
-      };
+  const noteLimits = {
+    presentation: { min: 0, max: 9 },
+    cuissonPrincipale: { min: 0, max: 7 },
+    cuissonGarniture: { min: 0, max: 7 },
+    accordGlobal: { min: 0, max: 7 },
+  };
 
-      // Stockage des notes sans écraser les notes déja présentes dans la base de donnée
-      // On récupére les notes déja présentes. Ensuite, on traite la promesse obtenue et on applique la fonction save_notes
-      // La fonction save_notes permet d'ajouter une instance de notes d'un candidat
-      const save_notes = (
-        all_notes: Record<string, any>,
-        candidate: string,
-        candidates_notes: Object
-      ) => {
-        all_notes["candidat" + candidate] = candidates_notes;
-        store.set("notes", all_notes);
-      };
-      store.get("notes").then((all_notes: Record<string, any>) => {
-        save_notes(all_notes, candidate, candidates_notes);
-        setValidateNote(true);
-        history.push("/listingdegustation");
-      });
+  // Fonction pour valider une note avec ses limites
+  const isValidNote = (note: string, min: number, max: number) => {
+    const noteValue = parseFloat(note);
+    return !isNaN(noteValue) && noteValue >= min && noteValue <= max;
+  };
+
+  // Fonction de validation pour toutes les notes
+  const validateNotes = () => {
+    return (
+      isValidNote(
+        values.presentation,
+        noteLimits.presentation.min,
+        noteLimits.presentation.max
+      ) &&
+      isValidNote(
+        values.cuissonPrincipale,
+        noteLimits.cuissonPrincipale.min,
+        noteLimits.cuissonPrincipale.max
+      ) &&
+      isValidNote(
+        values.cuissonGarniture,
+        noteLimits.cuissonGarniture.min,
+        noteLimits.cuissonGarniture.max
+      ) &&
+      isValidNote(
+        values.accordGlobal,
+        noteLimits.accordGlobal.min,
+        noteLimits.accordGlobal.max
+      )
+    );
+  };
+
+  // Gestion de la validation et de l'enregistrement des notes
+  const handleValidateClick = () => {
+    if (validateNotes()) {
+      if (store) {
+        let candidates_notes = {
+          presentation: values.presentation,
+          cuissonPrincipale: values.cuissonPrincipale,
+          cuissonGarniture: values.cuissonGarniture,
+          accordGlobal: values.accordGlobal,
+          total: total,
+          observations: observations,
+        };
+
+        const save_notes = (
+          all_notes: Record<string, any>,
+          candidate: string,
+          candidates_notes: Object
+        ) => {
+          all_notes["candidat" + candidate] = candidates_notes;
+          store.set("notes", all_notes);
+        };
+
+        store.get("notes").then((all_notes: Record<string, any>) => {
+          save_notes(all_notes, candidate, candidates_notes);
+          setValidateNote(true);
+          history.push("/listingdegustation");
+        });
+      }
+    } else {
+      setAlertInvalideNotes(true); // Affiche l'alerte si une note est invalide
     }
   };
 
@@ -162,16 +205,16 @@ function TableEvaluationDegustation() {
         </div>
         <IonGrid fixed={true}>
           <CustomNotesInput
-            min={0}
-            max={9}
+            min={noteLimits.presentation.min}
+            max={noteLimits.presentation.max}
             onIonInput={(value) => handleInputChange("presentation", value)}
             value={values.presentation}
             noteLabel="Présentation générale et netteté du contenant"
           ></CustomNotesInput>
 
           <CustomNotesInput
-            min={0}
-            max={7}
+            min={noteLimits.cuissonPrincipale.min}
+            max={noteLimits.cuissonPrincipale.max}
             onIonInput={(value) =>
               handleInputChange("cuissonPrincipale", value)
             }
@@ -180,16 +223,16 @@ function TableEvaluationDegustation() {
           ></CustomNotesInput>
 
           <CustomNotesInput
-            min={0}
-            max={7}
+            min={noteLimits.cuissonGarniture.min}
+            max={noteLimits.cuissonGarniture.max}
             onIonInput={(value) => handleInputChange("cuissonGarniture", value)}
             value={values.cuissonGarniture}
             noteLabel="Cuisson et qualité gustative des garnitures"
           ></CustomNotesInput>
 
           <CustomNotesInput
-            min={0}
-            max={7}
+            min={noteLimits.accordGlobal.min}
+            max={noteLimits.accordGlobal.max}
             onIonInput={(value) => handleInputChange("accordGlobal", value)}
             value={values.accordGlobal}
             noteLabel="Accord entre les garnitures et la pièce principale"
@@ -233,6 +276,13 @@ function TableEvaluationDegustation() {
                 showAlert={validateNote}
                 setShowAlert={setValidateNote}
                 message={"Les notes ont été correctement enregistrées"}
+              ></Alert>
+              <Alert
+                showAlert={alertInvalideNotes}
+                setShowAlert={setAlertInvalideNotes}
+                message={
+                  "Attention, une des notes n'est pas valide et n'est pas prise en compte dans le calcul. Merci de la corriger."
+                }
               ></Alert>
               <span
                 className="header-footer"
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Criteria = {
+    label: string;
+    description: string;
+    value: string;
+    min: number;
+    max: number;
+  };
